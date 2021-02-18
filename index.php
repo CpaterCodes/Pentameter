3229baaebbffe9851db4293f442b7051dc3a18d0
Charlie Paterson <Charlieed.paterson@googlemail.com>
2021-02-18 19:03:43 +0000

    Chore: Set up index page for interface implementation, tests and functionality pending

---
+++ index.php
@@ -0,0 +1,13 @@
+<head>
+  <?php
+    require 'src/StarAngles.php';
+    require 'src/StarLines.php';
+  ?>
+  <title>Pentameter: A Pentacle helper!</title>
+  <meta charset="utf-8">
+</head>
+
+<body>
+  Hello World!<br>
+  <?php echo "And hello again!"; ?>
+</body>
