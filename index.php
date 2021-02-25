fbe997f85d29892e21249787e7ecf10af044bd14
Charlie Paterson <Charlieed.paterson@googlemail.com>
2021-02-25 14:49:55 +0000

    Style: Establish a basic and eyecatching style for the page

--- index.php
+++ index.php
@@ -3,21 +3,25 @@
     require 'src/PageHelpers.php';
   ?>
   <title>Pentameter: A Pentacle helper!</title>
+  <link rel="stylesheet" href="assets/style.css">
   <meta charset="utf-8">
 </head>
 
 <body>
+  <h1>Pentameter</h1>
+  <h3>Fill in the boxes as instructed to get useful metrics</h3>
+
   <div id='input-values'>
    <form action="submit" method="post">
-     Vertices: <input name="vertex-count" placeholder="Enter a value" type="text"><br>
-     Circumference: <input name="circumference" placeholder="(Optional)" type="text"><br>
+     <input name="vertex-count" placeholder="Enter Vertices count here" type="text"><br>
+     <input name="circumference" placeholder="Optionally, enter circumference here" type="text"><br>
      <input type="submit" value="Calculate">
    </form>
  </div>
 
  <div id='output-values'>
    Vertex Angle:
-   <?php echo handleAngleInput(); ?><br>
+   <?php echo handleAngleInput(); ?><br><br>
    Line Length:
    <?php echo handleLineInput(); ?>
  </div>
